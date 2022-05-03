d8d22d88a694ea4b0ada96acbf7be8b50f9898c3
Brooks Lyrette <brooks@lyrette.ca>
2022-05-03 09:32:18 -0400

    Add tests so we can setup CI

---
+++ components/__tests__/FileList.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import FileList from '../FileList'
+import '@testing-library/jest-dom'
+
+describe('Home', () => {
+  it('renders a loading state', () => {
+    render(<FileList files={[]} state="loading" />)
+
+    const heading = screen.getByText('Loading...')
+
+    expect(heading).toBeInTheDocument()
+  })
+  it('renders a files once loaded', () => {
+    render(<FileList files={[
+      {
+        name: 'file1.tgz',
+        size: 100,
+        lastModified: new Date(Date.now() - 1000)
+      }
+    ]} state="loaded" />)
+
+    const fileItem = screen.getByText('file1.tgz')
+
+    expect(fileItem).toBeInTheDocument()
+  })
+})
---
+++ components/__tests__/Header.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import Header from '../Header'
+import '@testing-library/jest-dom'
+import { Role } from '../../lib/model/model'
+
+describe('Home', () => {
+  it('renders a heading when unauthenticated', () => {
+    render(<Header session={null}/>)
+
+    const heading = screen.getByText('Log in')
+
+    expect(heading).toBeInTheDocument()
+  })
+  it('renders a heading when authenticated', () => {
+    const role: Role = 'support'
+    const mockSession = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: { username: "tester", name: "tester", role: role }
+    };
+
+    render(<Header session={mockSession}/>)
+
+    const heading = screen.getByText('Logged in as tester,')
+    const logout = screen.getByText('Log out')
+
+    expect(heading).toBeInTheDocument()
+    expect(logout).toBeInTheDocument()
+  })
+})
