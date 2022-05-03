5a177b1137b48df4e0a978b4d787c1497ba8d84c
Brooks Lyrette <brooks@lyrette.ca>
2022-05-03 11:06:08 -0400

    Mock fetch and mock server for unit testing

---
+++ pages/__tests__/uploads.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import '@testing-library/jest-dom'
+
+import Uploads from '../uploads'
+
+jest.mock("next-auth/react")
+
+const server = setupServer(
+  rest.get('/api/storage/list', (req, res, ctx) => {
+    return res(ctx.json([
+      {
+        name: 'file1.tgz',
+        size: 100,
+        lastModified: new Date(Date.now() - 1000)
+      }
+    ]))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe('Upload', () => {
+  it('renders when loading', async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: {
+        user: {
+          username: 'tester',
+          role: 'support'
+        },
+      },
+      status: 'authenticated',
+    })
+    render(<Uploads />)
+
+    // check for loading state
+    const heading = screen.getByText("Loading...")
+    expect(heading).toBeInTheDocument()
+
+    // ensure file list is loaded
+    await waitFor(() => screen.getByText('file1.tgz'))
+  })
+})
